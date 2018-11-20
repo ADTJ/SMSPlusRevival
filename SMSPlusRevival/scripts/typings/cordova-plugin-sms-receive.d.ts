8fa112cf149ec1a4606f5b1e5b1c626c7cb281e1
Nadir Hanid <nadir@nadirhanid.uk>
2018-11-20 11:13:15 +0000

    created typescript definition file for the sms receive plugin.

---
+++ SMSPlusRevival/scripts/typings/cordova-plugin-sms-receive.d.ts
@@ -0,0 +1,6 @@
+﻿export module SMSReceive {
+
+    export function startWatch(onSuccess?: () => void, onError?: (error?: any) => void): void;
+    export function stopWatch(onSuccess?: () => void, onError?: (error?: any) => void): void;
+
+}
